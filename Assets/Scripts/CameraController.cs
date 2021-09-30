ba5b1aa6286e5b4f84241da8e08845d89b5d1aab
YaRu056 <yaru1010514@gmail.com>
2021-09-30 15:30:59 +0800

    BallGame

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject playerObject;
+    private Vector3 camOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOffset = this.transform.position - playerObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = playerObject.transform.position + camOffset;
+    }
+}
